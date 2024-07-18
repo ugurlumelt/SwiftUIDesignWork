cd46253597ab91a4905b97414e3529252f2720da
Meltem <meltemak8@gmail.com>
2024-07-18 15:03:34 +0200

    Initial Commit

---
+++ SwiftUIDesignWork1/SwiftUIDesignWork1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignWork1App.swift
+//  SwiftUIDesignWork1
+//
+//  Created by Meltem Uğurlu on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignWork1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
