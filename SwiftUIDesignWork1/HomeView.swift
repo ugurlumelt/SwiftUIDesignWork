29b6499bb34a9610cfb24c77bd5de4a6de75edda
Meltem <meltemak8@gmail.com>
2024-07-18 15:10:32 +0200

    Added new view

---
+++ SwiftUIDesignWork1/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftUIDesignWork1
+//
+//  Created by Meltem Uğurlu on 18.07.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
